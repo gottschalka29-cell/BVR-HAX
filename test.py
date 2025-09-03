627962a8ec29d29f98a459922de3292e6c98f836
gottschalka29-cell <gottschalka29@bcdschool.org>
2025-09-03 20:21:32 +0000

    test run

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
 print("Hellow world")
+
+# string = "bob"
+# integer = 1000
+# float = 0.5
+# boolean = False
+
+# length = 2
+# width = 3
+
+# length= int(input("whats the length"))
+
+# width = int(input("whats the width"))
+
+# print(length+width+length+width)
+
+# print(length*width)
+
+# verb = input("gimme a verb")
+# noun = input("gimme a noun")
+# adjective = input("gimme an adjective")
+
+# print("There was a guy named bob who was very " + adjective + " he was " + noun + " he was " + verb)
