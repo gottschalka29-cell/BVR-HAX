b0ec34bf2f6f42035267f726a437a22199137a42
gottschalka29-cell <gottschalka29@bcdschool.org>
2025-09-11 14:54:44 +0000

    w

--- guessing.py
+++ guessing.py
@@ -1,31 +1,31 @@
 
-import random
+# import random
 
-def lets_play(guesses):
-    print("This program will create a number and you have to guess it")
-    print("You have howevermany guesses you put in. (I DONT KNOW HOW TO CODE THIS RIGHT SORRY.)") # try to fix 
-    guesses_used = 0 
-    number = random.randint(1,1000) # cough i cheated 
-    print(number)
-    for i in range(guesses):
-        print("Whats your guess?")
-        guess = input(int())
-        print(guesses_used)
-        if (int(guess) < number):
-            print("Your answer is too low. Below is how many gueses you have left")
-            guesses_used += 1 
-            print(guesses - guesses_used)
-        if(int(guess) > number): 
-            print("Your answer is too high. Below is how many gueses you have left")
-            guesses_used += 1 
-            print(guesses - guesses_used)
-        if(guesses_used == guesses):
-            print("shucks you lost")
-            print(number)
-        if(int(guess) == number):
-            print("You did it!")
-            break 
+# def lets_play(guesses):
+#     print("This program will create a number and you have to guess it")
+#     print("You have however many guesses you put in. (I DONT KNOW HOW TO CODE THIS RIGHT SORRY.)") # try to fix 
+#     guesses_used = 0 
+#     number = random.randint(1,1000) # cough i cheated 
+#     print(number)
+#     for i in range(guesses):
+#         print("Whats your guess?")
+#         guess = input(int())
+#         print(guesses_used)
+#         if (int(guess) < number):
+#             print("Your answer is too low. Below is how many gueses you have left")
+#             guesses_used += 1 
+#             print(guesses - guesses_used)
+#         if(int(guess) > number): 
+#             print("Your answer is too high. Below is how many gueses you have left")
+#             guesses_used += 1 
+#             print(guesses - guesses_used)
+#         if(guesses_used == guesses):
+#             print("shucks you lost")
+#             print(number)
+#         if(int(guess) == number):
+#             print("You did it!")
+#             break 
      
     
 
-lets_play(5)
+# lets_play(5)
